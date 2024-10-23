8133eb8367d1c04a4621db6d7a1a8afeae97682f
Nyller <edu.sampaio1777@gmail.com>
2024-10-23 12:23:29 -0300

    Inclusão e estilização de página de login e ajuste na página Home.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Olá, Mundo!'
+    return render_template('home.html')
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
@@ -15,6 +15,10 @@ def cadastro():
         password = request.form.get('password')
         return f'Olá, {name}  {lastname}! Seja bem-vindo!'
     return render_template('cadastro.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
